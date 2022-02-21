61b4622c1e62f8c1e4034d36b9bbbf430e6ee260
Zoe Fisher <zrfisher@usc.edu>
2022-02-20 17:17:02 -0800

    make api request to group 3's server

--- server/routes.js
+++ server/routes.js
@@ -4,6 +4,7 @@ const { check, validationResult, matchedData } = require('express-validator');
 
 const csrf = require('csurf');
 const csrfProtection = csrf({ cookie: true });
+const axios = require('axios');
 
 const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() });
@@ -12,14 +13,6 @@ router.get('/', (req, res) => {
   res.render('index');
 });
 
-// router.get('/contact', csrfProtection, (req, res) => {
-//   res.render('contact', {
-//     data: {},
-//     errors: {},
-//     csrfToken: req.csrfToken()
-//   });
-// });
-
 router.get('/haptics_configuration', csrfProtection, (req, res) => {
   res.render('haptics_configuration', {
     data: {},
@@ -27,38 +20,6 @@ router.get('/haptics_configuration', csrfProtection, (req, res) => {
     csrfToken: req.csrfToken()
   });
 });
-
-// router.post('/contact', upload.single('photo'), csrfProtection, [
-//   check('message')
-//     .isLength({ min: 1 })
-//     .withMessage('Message is required')
-//     .trim(),
-//   check('email')
-//     .isEmail()
-//     .withMessage('That email doesn‘t look right')
-//     .bail()
-//     .trim()
-//     .normalizeEmail(),
-// ], (req, res) => {
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     return res.render('contact', {
-//       data: req.body,
-//       errors: errors.mapped(),
-//       csrfToken: req.csrfToken()
-//     });
-//   }
-//
-//   const data = matchedData(req);
-//   console.log('Sanitized: ', data);
-//   //console.log('Gesture: ', req.body.gesture)
-//   if (req.file) {
-//     console.log('Uploaded: ', req.file);
-//   }
-//
-//   req.flash('success', 'Thanks for the message! I‘ll be in touch :)');
-//   res.redirect('/');
-// });
 
 router.post('/haptics_configuration', upload.single('haptics_file'), csrfProtection, [
   check('instructions')
@@ -82,14 +43,21 @@ router.post('/haptics_configuration', upload.single('haptics_file'), csrfProtect
   }
 
   const data = matchedData(req);
-  console.log(req.body.email);
-  console.log(req.body.instructions);
-  console.log(req.body.gesture)
-  if (req.file) {
-    console.log('Uploaded: ', req.file);
+
+  try {
+    axios.post("https://haptics-test.herokuapp.com/config/setConfig", {
+      "config_id": req.body.email,
+      "config_body": {
+        "gesture": req.body.gesture,
+        "instructions": req.body.instructions,
+        "haptic_file": req.file
+      }
+    });
+    req.flash('success', 'Thanks for the haptics configuration!');
+  } catch {
+    req.flash('error', 'Please try again.');
   }
 
-  req.flash('success', 'Thanks for the haptics configuration!');
   //insert generated participant code in message
   res.redirect('/');
 });
